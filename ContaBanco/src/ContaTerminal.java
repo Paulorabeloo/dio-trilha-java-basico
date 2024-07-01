edef5e318924ca1d76200d6b161d79256d25d875
Paulo Rabelo <pauloeduardorabelomiranda@gmail.com>
2024-07-01 07:23:08 -0300

    Conta Banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal 
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o dígito da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu saldo bancário: ");
+        float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta número " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
